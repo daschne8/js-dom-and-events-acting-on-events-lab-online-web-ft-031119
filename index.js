466a6a9c37898364bf25274cad9bed17f0376d4b
Daniel Schneider <daschne8@gmail.com>
2019-06-18 16:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 function retrieveEmployeeInformation(){
-  return document.body.querySelector('input')
+  return document.body.querySelector('input').innerHTML
 }
